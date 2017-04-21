45566bb2c4a40214e23d6ff2a0cb7c2f33aa0367
disrupted <salomon.popp@gmail.com>
2017-04-21 14:12:39 +0200

    Theatre2

---
+++ src/Theatre2.java
@@ -0,0 +1,11 @@
+public class Theatre2 extends Theatre
+{
+    public Theatre2() {
+        super();
+        name = "Theatre2";
+        addSeatRow(5);
+        addSeatRow(5);
+        addSeatRow(4);
+        addSeatRow(1);
+    }
+}
